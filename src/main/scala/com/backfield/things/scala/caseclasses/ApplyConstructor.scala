dbab181f933c6ebb6c80f507aa56efeeaf13bf2a
Joshua Backfield <suroot.chmod777@gmail.com>
2015-12-27 16:16:59 -0500

    Abstracting some functionality into the ExplainCode library.

--- src/main/scala/com/backfield/things/scala/caseclasses/ApplyConstructor.scala
+++ src/main/scala/com/backfield/things/scala/caseclasses/ApplyConstructor.scala
@@ -1,23 +1,25 @@
 package com.backfield.things.scala.caseclasses
 
-import com.backfield.explain.ExplainCode
-import com.backfield.things.scala.Examples
+import com.backfield.explain.{Example, ExplainCode}
 
-object ApplyConstructor extends Examples {
+object ApplyConstructor extends Example {
 
   var done = false
 
   override val name = "Apply Constructor"
 
+  implicit val ec = ExplainCode()
+
   override def execute() : Unit = {
-    ExplainCode.explain("TestCaseClass(\"Foo\", 1234)")(TestCaseClass("Foo", 1234), true, (_ : TestCaseClass) => {
+    ExplainCode.explain("TestCaseClass(\"Foo\", 1234)", TestCaseClass("Foo", 1234), true, (_ : TestCaseClass) => {
       println("Becomes an apply")
       ExplainCode.explain(TestCaseClass("Foo", 1234), true, (tcc : TestCaseClass) => {
         println(s"Field 1: ${tcc.field1}")
         println(s"Field 2: ${tcc.field2}")
         println(s"TestCaseClass.toString: $tcc")
-      })
-    })
+      },
+      ec)
+    }, ec)
     done = true
   }
 
--- src/main/scala/com/backfield/things/scala/caseclasses/CopyConstructor.scala
+++ src/main/scala/com/backfield/things/scala/caseclasses/CopyConstructor.scala
@@ -1,28 +1,29 @@
 package com.backfield.things.scala.caseclasses
 
-import com.backfield.explain.ExplainCode
-import com.backfield.things.scala.Examples
+import com.backfield.explain.{Example, ExplainCode}
 
-object CopyConstructor extends Examples {
+object CopyConstructor extends Example {
 
   var done = false
 
   override val name = "Copy Constructor"
 
+  implicit val ec = ExplainCode()
+
   override def execute() : Unit = {
 
     ExplainCode.explain[TestCaseClass](new TestCaseClass("Foo", 1234), true, (tcc : TestCaseClass) => {
       println(s"TestCaseClass: $tcc")
-      ExplainCode.explain[TestCaseClass]("tcc.copy(field1 = \"Bar\")")(tcc.copy(field1 = "Bar"), true, (changed : TestCaseClass) => {
+      ExplainCode.explain[TestCaseClass]("tcc.copy(field1 = \"Bar\")", tcc.copy(field1 = "Bar"), true, (changed : TestCaseClass) => {
         println(s"Only Field1 Changed: $changed")
-      })
-      ExplainCode.explain[TestCaseClass]("tcc.copy(field2 = 5678)")(tcc.copy(field2 = 5678), true, (changed : TestCaseClass) => {
+      }, ec)
+      ExplainCode.explain[TestCaseClass]("tcc.copy(field2 = 5678)", tcc.copy(field2 = 5678), true, (changed : TestCaseClass) => {
         println(s"Only Field2 Changed: $changed")
-      })
+      }, ec)
       ExplainCode.explain[TestCaseClass](tcc.copy("Bar", 5678), false, (changed : TestCaseClass) => {
         println(s"Both fields changed: $changed")
-      })
-    })
+      }, ec)
+    }, ec)
     done = true
   }
 
